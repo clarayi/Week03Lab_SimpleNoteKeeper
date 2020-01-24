711bdc56c15203196d0b59e07b80da5c036e6b0b
clarayi <59713032+clarayi@users.noreply.github.com>
2020-01-23 20:01:33 -0700

    Note.java Edited The constructors, getters, and setters are added to Note.java class.

--- src/java/domain/Note.java
+++ src/java/domain/Note.java
@@ -1,15 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
 /**
- *
+ * This is Note class.
  * @author 810783
  */
 public class Note
 {
+    private String title;
+    private String content;
     
+    public Note()
+    {
+        this.title = "";
+        this.content = "";
+    }
+    
+    public Note(String title, String content)
+    {
+        this.title = title;
+        this.content = content;
+    }
+    
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+    
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+    
+    public String getTitle()
+    {
+        return this.title;
+    }
+    
+    public String getContent()
+    {
+        return this.content;
+    }
 }
